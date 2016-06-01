03ca08e8be14d7330cf3aa4f84308da386f9b348
Zaicon Kiroshu <kzaicon@yahoo.com>
2016-06-01 06:20:34 -0500

    API 1.24 Update (reverted from commit 2b2880dc63f89e310b05a840c6c07f0d08056b5e)

--- PermissionControl/PermControl.cs
+++ PermissionControl/PermControl.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 
 namespace PermissionControl
 {
-    [ApiVersion(1,24)]
+    [ApiVersion(1,23)]
     public class PermControl : TerrariaPlugin
     {
         public override string Name { get { return "PermControl"; } }
         public override string Author { get { return "Zaicon"; } }
         public override string Description { get { return "Searches for commands/permissions within groups."; } }
-        public override Version Version { get { return Assembly.GetExecutingAssembly().GetName().Version; } }
+        public override Version Version { get { return new Version(1, 3, 3, 0); } }
 
         public PermControl(Main game)
             : base(game)
